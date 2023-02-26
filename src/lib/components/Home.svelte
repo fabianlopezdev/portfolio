5d34bdeb0aa0976a2b42403a42d5dcb36c14dd9b
Fabian Lopez Gonzalez <fabianlopez@pm.me>
2023-02-26 09:07:37 -0500

    Button Download Resume to work in build

--- src/lib/components/Home.svelte
+++ src/lib/components/Home.svelte
@@ -12,7 +12,7 @@
 	<h1 style="color: #016AA1">Hello, I am Fabian!</h1>
 	<div style="display: flex; gap: 1em;">
 		<Button --color="blue" on:click={scrollTo} href={'#contact'} icon={'envelope'}/>
-		<Button --color="black" href={'/Fabian-Lopez-Resume.pdf'} icon={''}>Resume</Button>
+		<Button --color="black" href={'https://portfolio-fabs-and.vercel.app/Fabian-Lopez-Resume.pdf'} icon={''}>Resume</Button>
 	</div>
 </section>
 
