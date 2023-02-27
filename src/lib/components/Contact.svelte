1b1e911373aa97ba1c795d7bf079246da7031e5f
Fabian Lopez Gonzalez <fabianlopez@pm.me>
2023-02-27 18:38:12 -0500

    Working on contact page

--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -1,3 +1,9 @@
+<script>
+	import EnvelopeIcon from 'svelte-icons/fa/FaRegEnvelope.svelte';
+
+	import GoCalendar from 'svelte-icons/go/GoCalendar.svelte'
+</script>
+
 <section id="contact">
 	<h2>
 		Tell me about your project
@@ -17,36 +23,102 @@
 
 			<label for="message"></label>
 			<textarea id="message" cols="60" rows="10" placeholder=" How can I help you?" />
+			<button>
+				Submit
+			</button>
 		</form>
 	</div>
-</section>
+	<div class='articles-container'>
+		<article>
+			<div class='icons'>
+				<EnvelopeIcon/>
+			</div>
+			<div>
+				Send me an email:
+				<br>
+				<a class='email'>
+					fabianlopez@pm.me
+				</a>
+			</div>
+		</article>
+		<article><div class='icons'>
+				<GoCalendar/>
+			</div>
+			<div>
+				Schedule a cal with Calendly:
+				<br>
+				<a class='email'>
+					/fabian-lopez-gonzalez
+				</a>
+			</div></article>
+	</div>
+	</section>
 
 <style>
+	.articles-container {
+		display: flex; 
+		gap: 1rem;
+		margin-top:1rem;
+	}
+	.icons{
+		width:3rem;
+		height: 3rem;
+	}
+	.email {
+		font-size: 1.15rem;
+		font-weight: 700;
+	}
+	article {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+	}
 	section {
-		min-height: 100vh;
+		height: 100vh;
+		height: 100svh;
 		display: flex;
 		flex-direction: column;
-		justify-content: center;
+		justify-content: space-evenly;
 		align-items: center;
 	}
+	button {
+		position: absolute;
+		/* bottom: 0; */
+		margin-top: 23.5rem;
+		text-align: center;
+		width: 215px;
+		padding: 1rem;
+		border-radius: 1rem;
+		font-size: 15.8px;
+		font-weight: 700;
+		border: none;
+		color: var(--clr-font-contrast);
+		background-color: var(--clr-accent);
+	}
+	button:hover {
+		cursor: pointer
+	}
 	.form-container {
+		position: relative;
 		border: 0.1rem solid var(--clr-secondary);
-		border-radius: 0.5em;
+		border-radius: 1em;
 		padding: 2rem;
 		display: inline-block;
 	}
 	form {
 		display: flex;
 		flex-direction: column;
+		align-items:center;
 		gap: 1rem;
 	}
 	input {
 		height: 2rem;
-		border-radius: 0.5rem;
+		border-radius: 1rem;
 		border: none;
+		width: fit-content;
 	}
 	textarea {
-		border-radius: 0.5rem;
+		border-radius: 1rem;
 		border: none;
 	}
 </style>
---
+++ src/lib/components/SharingCard.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let title;
+  export let image;
+  export let link;
+</script>
+
+<article>
+  <img src=''/>
+</article>
