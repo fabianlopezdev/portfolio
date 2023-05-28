d0cab594a500675e9dadc3c8c87d90d7df1e91df
Fabian Lopez Gonzalez <fabianlopez@pm.me>
2023-05-28 09:15:26 -0400

    Projects: Switch: re-styled it. Added underline instead of dot

--- src/lib/components/projects/Switch.svelte
+++ src/lib/components/projects/Switch.svelte
@@ -1,61 +1,74 @@
 <div class="toggle">
   <input type="radio" id="project" name="toggle" checked>
-  <label class='dale'for="project">Projects</label>
+  <label for="project"><span class='label-text'>Projects</span></label>
   <input type="radio" id="skills" name="toggle">
-  <label for="skills">Skills</label>
-  <span class="slider"><span class="dot"></span></span>
+  <label for="skills"><span class='label-text'>Skills</span></label>
+  <span class="slider"></span>
 </div>
 
-
 <style>
-  label {
-    /* width: 100px; */
-    /* border-radius: 6px; */
-    /* z-index: 10; */
-    padding: 0;
-  }
-
   .toggle {
-    /* padding:0; */
     position: relative;
     display: flex;
     max-width: 215px;
     width: 15rem;
-    font-size: 15.8;
+    font-size: 15.8px;
     font-weight: 700;
     border-radius: 1rem;
     overflow: hidden;
     border: 1px solid black;
     background-color: #ddd;
-    margin-left: 1rem;
     justify-content: space-between;
     align-items: center;
   }
 
-.toggle input[type="radio"] {
+  .toggle input[type="radio"] {
     display: none;
-}
+  }
 
-.toggle label {
+  .toggle label {
+    position: relative;
+    overflow: hidden;
     padding: 10px 20px;
     cursor: pointer;
     color: #000;
-    flex-grow: 1;
-    text-align: center;
     z-index: 1;
     transition: color 0.2s;
-     border-radius: 1rem;
-     flex: 0 0 45%;
-     letter-spacing: 1px;
-    /* border: 3px solid #d0dff0; */
-}
-
-#project:checked ~ label[for="project"],
-#skills:checked ~ label[for="skills"] {
-    color: black;
-    
+    border-radius: 1rem;
+    flex: 0 0 45%;
+    letter-spacing: 1px;
+    text-align: center;
   }
-  
+
+  .toggle .label-text {
+    position: relative;
+    display: inline-block;
+  }
+
+  .toggle .label-text::after {
+    content: "";
+    position: absolute;
+    bottom: -17%;
+    left: 0;
+    right: 0;
+    height: 2px;
+    background: blue;
+    transition: transform 0.5s;
+    transform: scaleX(0);
+    transform-origin: center;
+    border-radius: 1rem;
+  }
+
+  #project:checked ~ label[for="project"] .label-text::after,
+  #skills:checked ~ label[for="skills"] .label-text::after {
+    transform: scaleX(1);
+  }
+
+  #project:checked ~ label[for="project"],
+  #skills:checked ~ label[for="skills"] {
+    color: black;
+  }
+
   .slider {
     position: absolute;
     background: #f9fcfd;
@@ -67,42 +80,31 @@
     z-index: 0;
     border-radius: 1rem;
     color: blue;
-      display: flex;
-  align-items: center;
-  justify-content: flex-start;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
     border: 1px solid black;
-     flex: 0 0 10%;
-    /* box-sizing:content-box; */
-    /* padding-left: 1rem; */
-     /* margin-right: 100px; */
-    /* z-index:1; */
-}
+    flex: 0 0 10%;
+  }
 
-#skills:checked ~ .slider {
+  #skills:checked ~ .slider {
     left: 50%;
-}
-.toggle input[type="radio"]:not(:checked) + label {
-    color: #888; /* or whatever color you want for the disabled look */
-    background-color: #ddd; /* or whatever background color you want for the disabled look */
-}
+  }
 
-.dot {
-  position: absolute;
-  width: 8px;
-  height: 8px;
-  background: blue; /* choose your desired color */
-  border-radius: 50%;
-  margin-left: 0.4rem;
-}
-label[for="project"] {
-  padding-left: 19px;
-}
+  .toggle input[type="radio"]:not(:checked) + label {
+    color: #888;
+    background-color: #ddd;
+  }
 
-label[for="skills"] {
-  padding-right: 35px;
-}
-#skills:checked ~ .slider .dot {
-  transform: translateX(100%);
-}
+  #skills:not(:checked) ~ label[for="skills"] .dot {
+    display:none;
+  }
 
+  label[for="project"] {
+    padding-left: 17px;
+  }
+
+  #skills:checked ~ .slider .dot {
+    transform: translateX(100%);
+  }
 </style>
