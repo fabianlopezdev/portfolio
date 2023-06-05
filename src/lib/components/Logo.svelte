6d23bd0deab8b5e1fcf808d7cfa83cc8c92054d0
Fabian Lopez Gonzalez <fabianlopez@pm.me>
2023-06-05 14:01:57 -0400

    Logo: testing animations

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -32,21 +32,45 @@
   }, 1000);
 </script>
 
-<div>
-{#if showSvg}
 
-	<svg height="100" stroke="black" stroke-width="2" class="text-line" width="100%"
+<div class='logo'>
+{#if showSvg}
+	
+
+	<svg height="60" stroke="black" stroke-width="2" class="text-line" width="50%"
 	><text x="50%" dominant-baseline="middle" text-anchor="middle" y="50%">fab</text></svg
 	>
+	<span class='dot'>.</span>
 	{/if}
 </div>
+
+
+
 <style>
+
+	.logo {
+		position: relative;
+	}
+
+	.dot {
+		position: absolute;
+		color: blue;
+		font-size: 4rem;
+		bottom: 1px;
+		left: 4.1rem;
+	}
 .text-line {
   stroke-dasharray: 500;
   stroke-dashoffset: 500;
-  animation: dash 2s linear forwards, filling 0.5s ease-in forwards;
+  animation: dash 4s linear forwards, filling 0.5s ease-in forwards;
 	/* animation-delay: 3s; */
   font-size: 3rem;
+	/* display: inline-block; */
+	position: relative;
+	width: 4rem;
+	height: 3rem;
+	margin-top: 0.8rem;
+	margin-left: 0.3rem;
 }
 
 .text-line text {
@@ -54,6 +78,8 @@
   font-weight: normal;
   font-style: normal;
 	/* letter-spacing: -2px; */
+	/* position: absolute;
+	left: 0; */
 }
 
 @keyframes dash {
